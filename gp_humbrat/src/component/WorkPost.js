74b45237a1fa4e5be777816331116af2a8231a33
akshay.waingankar95@gmail.com <akshay.waingankar95@gmail.com>
2020-08-13 01:23:25 +0530

    Images are displayed. 3 cards in one row

--- gp_humbrat/src/component/WorkPost.js
+++ gp_humbrat/src/component/WorkPost.js
@@ -8,59 +8,65 @@ import {
   hashHistory,
 } from "react-router-dom";
 
+const WorkList = (props) => (
+  <CardDeck>
+    <Card>
+      <Card.Img
+        variant="top"
+        src={"work/" + props.workInfo.tbl_work_images_title}
+        className="img-thumbnail"
+      />
+      <Card.Body>
+        <Card.Title>{props.workInfo.tbl_work_images_title}</Card.Title>
+        <Card.Text>
+          {props.workInfo.tbl_work_images_title}
+          <Link to="/WorkDetails" className="nav-link">
+            सविस्तर माहिती
+          </Link>
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <small className="text-muted">{props.workInfo.tbl_work_date}</small>
+      </Card.Footer>
+    </Card>
+  </CardDeck>
+);
+
 export default class WorkPost extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      workPostArray: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:4500/humbrat/work_thumbnails")
+      .then((response) => {
+        this.setState({
+          workPostArray: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  workPostList() {
+    if (this.state.workPostArray.length > 0) {
+      return this.state.workPostArray.map(function (currentWorkInfo, i) {
+        return <WorkList workInfo={currentWorkInfo} key={i}></WorkList>;
+      });
+    } else {
+      return <h2>Data not found</h2>;
+    }
+  }
   render() {
     return (
       <div>
         <h2>Work post here</h2>
-        <CardDeck>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={"uploads/2020-07-30T15-34-36.338Z-road_inauguration.jpeg"}
-              className="img-thumbnail"
-            />
-            <Card.Body>
-              <Card.Title>अनंतवाडी रास्ता रुंदीकरण</Card.Title>
-              <Card.Text>
-                अनंतवाडी रास्ता रुंदीकरण दिनांक २७-०८/२०२० रोजी करण्यात आले
-                <Link to="/WorkDetails" className="nav-link">
-                  सविस्तर माहिती
-                </Link>
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This card has supporting text below as a natural lead-in to
-                additional content.{" "}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-        </CardDeck>
+        <div>{this.workPostList()}</div>
       </div>
     );
   }
