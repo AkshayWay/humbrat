c558ba27638516a6645a17db95cf04d83d7f5d0e
akshay.waingankar95@gmail.com <akshay.waingankar95@gmail.com>
2020-09-03 13:07:34 +0530

    Displaying village features in bootstrap Card is complete

--- gp_humbrat/src/component/Features.js
+++ gp_humbrat/src/component/Features.js
@@ -1,51 +1,57 @@
 import React, { component, Component } from "react";
+import axios from "axios";
 
 export default class Features extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      villageFeatureArray: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:4500/humbrat/village_features")
+      .then((response) => {
+        this.setState({
+          villageFeatureArray: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
   render() {
-    return (
-      <div>
-        <h1>Features here</h1>
+    let FeatureList = this.state.villageFeatureArray.map(function (
+      features,
+      i
+    ) {
+      return (
+        // <div className="col-6 col-md-4">
+        //   <WorkCard workPost={workPost} key={i} />
+        // </div>
         <div className="card mb-3" style={{ maxWidth: 100 + "%" }}>
           <div className="row no-gutters">
             <div className="col-md-4">
               <img
-                src={"work/2020-08-31T17-44-54.518Z-Bhorapi_vadi_2.jpeg"}
+                src={"feature/" + features.tbl_features_file_name}
                 class="card-img"
-                alt="..."
+                alt={features.tbl_features_file_name}
               />
             </div>
             <div className="col-md-8">
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <p className="card-text">
-                  <small class="text-muted">Last updated 3 mins ago</small>
-                </p>
+                <h4 className="card-title">{features.tbl_features_title}</h4>
+                <p className="card-text">{features.tbl_features_description}</p>
               </div>
             </div>
           </div>
         </div>
-        <div className="card mb-3" style={{ maxWidth: 100 + "%" }}>
-          <div className="row no-gutters">
-            <div className="col-md-8">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <p className="card-text">
-                  <small class="text-muted">Last updated 3 mins ago</small>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+      );
+    });
+    return (
+      <div>
+        <h1>Features here</h1>
+        {FeatureList}
       </div>
     );
   }
