69fc96513dfbd86fe076e12937e6b9db0f67cc28
akshay.waingankar95@gmail.com <akshay.waingankar95@gmail.com>
2020-09-04 13:34:38 +0530

    Village features completed

--- gp_humbrat/src/component/Features.js
+++ gp_humbrat/src/component/Features.js
@@ -8,6 +8,7 @@ export default class Features extends Component {
       villageFeatureArray: [],
     };
   }
+
   componentDidMount() {
     axios
       .get("http://localhost:4500/humbrat/village_features")
@@ -20,40 +21,50 @@ export default class Features extends Component {
         console.log(error);
       });
   }
-  render() {
-    let FeatureList = this.state.villageFeatureArray.map(function (
-      features,
-      i
-    ) {
-      return (
-        <div className="card mb-3" style={{ maxWidth: 100 + "%" }}>
-          <div className="row no-gutters">
-            <div
-              className="col-md-4"
-              style={{
-                display: features.tbl_features_file_name ? "inline" : "none",
-              }}
-            >
-              <img
-                src={"feature/" + features.tbl_features_file_name}
-                className="card-img"
-                alt={features.tbl_features_file_name}
-              />
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <h4 className="card-title">{features.tbl_features_title}</h4>
-                <p className="card-text">{features.tbl_features_description}</p>
+  FeatureList() {
+    if (this.state.villageFeatureArray.length > 0) {
+      return this.state.villageFeatureArray.map(function (features, i) {
+        return (
+          <div className="card mb-3" style={{ maxWidth: 100 + "%" }}>
+            <div className="row no-gutters">
+              <div
+                className="col-md-4"
+                style={{
+                  display: features.tbl_features_file_name ? "inline" : "none",
+                }}
+              >
+                <img
+                  src={"feature/" + features.tbl_features_file_name}
+                  className="card-img"
+                  alt={features.tbl_features_file_name}
+                />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h4 className="card-title">{features.tbl_features_title}</h4>
+                  <p className="card-text">
+                    {features.tbl_features_description}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
+        );
+      });
+    } else {
+      return (
+        <div>
+          <h5>वैशिष्ठ उपलब्ध नाही</h5>
         </div>
       );
-    });
+    }
+  }
+
+  render() {
     return (
-      <div>
+      <div style={{ minHeight: "calc(100vh - 70px)" }}>
         <h1>Features here</h1>
-        {FeatureList}
+        {this.FeatureList()}
       </div>
     );
   }
