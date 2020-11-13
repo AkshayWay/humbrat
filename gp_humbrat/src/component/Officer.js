6ccd058d32a5687a69c02a6719fac923edf87bab
akshay.waingankar95@gmail.com <akshay.waingankar95@gmail.com>
2020-11-14 00:29:07 +0530

    comments deleted with CSS changes

--- gp_humbrat/src/component/Officer.js
+++ gp_humbrat/src/component/Officer.js
@@ -50,16 +50,17 @@ class Office extends Component {
           width="1em"
           height="1em"
           viewBox="0 0 16 16"
+          color="black"
           class="bi bi-chevron-double-up"
           fill="currentColor"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708l6-6z"
           />
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
           />
         </svg>
@@ -70,16 +71,17 @@ class Office extends Component {
           width="1em"
           height="1em"
           viewBox="0 0 16 16"
+          color="black"
           class="bi bi-chevron-double-down"
           fill="currentColor"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
           />
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
           />
         </svg>
@@ -91,16 +93,17 @@ class Office extends Component {
           width="1em"
           height="1em"
           viewBox="0 0 16 16"
+          color="black"
           class="bi bi-chevron-double-up"
           fill="currentColor"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708l6-6z"
           />
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
           />
         </svg>
@@ -110,17 +113,18 @@ class Office extends Component {
         <svg
           width="1em"
           height="1em"
+          color="black"
           viewBox="0 0 16 16"
           class="bi bi-chevron-double-down"
           fill="currentColor"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
           />
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
           />
         </svg>
